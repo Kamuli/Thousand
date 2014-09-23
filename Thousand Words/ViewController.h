5f052f6817de6bfca32926e46e382155acd61043
Kamuli Software <info@kamuli.com>
2014-09-23 15:06:30 +0300

    Initial Commit

---
+++ Thousand Words/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thousand Words
+//
+//  Created by Juha Näppi on 23.9.2014.
+//  Copyright (c) 2014 Kamuli Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
