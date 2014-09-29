45e980f6beacd906430a57be073f6b38a06a537c
Kamuli Software <info@kamuli.com>
2014-09-29 20:02:14 +0300

    valmis

---
+++ PhotoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoDetailViewController.h
+//  Thousand Words
+//
+//  Created by Juha Näppi on 29.9.2014.
+//  Copyright (c) 2014 Kamuli Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoDetailViewController : UIViewController
+
+@end
---
+++ Thousand Words/FiltersCollectionCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FiltersCollectionCollectionViewController.h
+//  Thousand Words
+//
+//  Created by Juha Näppi on 29.9.2014.
+//  Copyright (c) 2014 Kamuli Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FiltersCollectionCollectionViewController : UICollectionViewController
+
+@end
